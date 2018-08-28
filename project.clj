e67c70d6078f83a10c535b84d8134f7b76c3d912
Stephen Nelson <stephen@montoux.com>
2018-08-28 14:20:30 +1200

    Version 1.0.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject montoux/test "1.0.0"
+(defproject montoux/test "1.0.1-SNAPSHOT"
   :description "Testing utilities for Clojurescript, including interactive HTML test output."
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
